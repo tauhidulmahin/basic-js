1cf378958315c9b51203c7c1252f707c997d3387
tauhidul-islam56 <tauhidulmahin@gmail.com>
2024-04-29 11:49:16 +0600

    inital commit

---
+++ index.js
@@ -0,0 +1,157 @@
+
+/*let x = 7;
+let y = 8;
+let z = x + y;
+console.log(z);
+
+let g = 50;
+let h = 27;
+let i = g - h;
+console.log(i);
+
+let a = 25;
+let b = 28;
+let c = a * b;
+console.log(c);
+
+let num1 = 10;
+let num2 = 7;
+let result = num1 < num2;
+console.log(result);*/
+
+
+
+
+/*const profile = {
+    name : "Tauhidul Islam",
+    posts : 120,
+    followers : 569000,
+    following : 120,
+    bio : "entrepreneur",
+    isFollow: true,
+};
+console.log(typeof profile["isFollow"]);*/
+
+
+
+
+/*let x = 12.23343;
+console.log(typeof x);
+
+
+let y = "hello world";
+console.log(typeof y);
+
+
+let a = 10==9;
+console.log(typeof a);
+
+
+let fruits = ["orrange","apple"];
+console.log(typeof fruits);
+
+
+let d;
+console.log(typeof d);
+
+
+let animal = null;
+console.log(typeof animal);*/
+
+
+
+
+
+/*var a = 20;
+var b = 30;
+a=50;
+result = a+b;
+console.log(result);
+
+
+let x = 25;
+let y = 35;
+x = 55;
+result = x+y;
+console.log(result);
+
+
+const pi = 3.1416;
+const g = 9.8;
+result = pi+g;
+console.log(result.toFixed(2));*/
+
+
+
+
+
+
+/*var x;
+x = prompt("Enter your name: ");
+document.write(x);
+
+
+var a=20;
+var b=a;
+console.log(a);
+console.log(b);
+a=30;
+console.log(a);
+console.log(b);
+
+
+var a = "12" + "hello";
+console.log(a);
+var b = "12" - "hello";
+console.log(b);
+
+
+var C,F;
+C = prompt("Enter a temperature");
+F = 9/5*C+32;
+alert(F);*/
+
+
+
+// Conditional Statements
+
+/*let number = 25;
+let result;
+
+if (number%2 ==0) {
+     result = "even" 
+} else {
+    result = "odd"
+}
+console.log(result);*/
+
+
+
+
+/*let num = prompt("enter a number : ");
+if (num % 5 == 0) {
+    result = "multiplied";
+} else {
+    result = "not-multiplied";
+}
+
+alert(result);*/
+
+
+
+/*let score = prompt("enter a number : ");
+if (score >= 80 && score <= 100) {
+    grade = "A"
+} else if (score >= 70 && score <= 79) {
+    grade = "B"
+}
+ else if (score >= 60 && score <= 69) {
+    grade = "C"
+}
+ else if (score >= 40 && score <= 59) {
+    grade = "D"
+} else {
+    grade = "FAIL"
+}
+
+alert(grade);*/
